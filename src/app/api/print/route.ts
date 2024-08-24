d4ca90a7f99764a3a6f222df7aef3dcacaef0a15
Dean <cheoljin.an@lotte.net>
2024-08-24 14:43:18 +0900

    fix: response type

--- src/app/api/print/route.ts
+++ src/app/api/print/route.ts
@@ -1,3 +1,4 @@
+// src/app/api/print/route.ts
 import { connect } from 'net';
 import { type NextRequest, NextResponse } from 'next/server';
 
@@ -8,33 +9,44 @@ interface PrintRequest {
   port: number;
 }
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     // Safely parse the JSON and assert its type
     const body: PrintRequest = await req.json();
-
     const { data, ip, port } = body;
 
-    return new Promise((resolve) => {
-      const conn = connect({
-        host: ip, // Use the IP from the request
-        port: port || 9100, // Use the provided port, or default to 9100
-        timeout: 3000,
-      }, () => {
-        conn.write(Buffer.from(new Uint8Array(data)), (err) => {
-          if (err) {
-            console.error('Printing error:', err);
-            resolve(NextResponse.json({ error: 'Printing failed' }, { status: 500 }));
-          } else {
-            resolve(NextResponse.json({ message: 'Printed successfully' }, { status: 200 }));
-          }
-          conn.destroy();
-        });
-      });
+    return new Promise<NextResponse>((resolve) => {
+      const conn = connect(
+        {
+          host: ip, // Use the IP from the request
+          port: port || 9100, // Use the provided port, or default to 9100
+          timeout: 3000,
+        },
+        () => {
+          conn.write(Buffer.from(new Uint8Array(data)), (err) => {
+            if (err) {
+              console.error('Printing error:', err);
+              resolve(
+                NextResponse.json({ error: 'Printing failed' }, { status: 500 })
+              );
+            } else {
+              resolve(
+                NextResponse.json(
+                  { message: 'Printed successfully' },
+                  { status: 200 }
+                )
+              );
+            }
+            conn.destroy();
+          });
+        }
+      );
 
       conn.on('error', (err) => {
         console.error('Connection error:', err);
-        resolve(NextResponse.json({ error: 'Connection failed' }, { status: 500 }));
+        resolve(
+          NextResponse.json({ error: 'Connection failed' }, { status: 500 })
+        );
       });
     });
   } catch (error) {
